10e294ca52a76711be554dabc0f2dc3959bf7ce3
kimgura <oli999@naver.com>
2025-04-02 09:05:57 +0900

    post-api 프로젝트 추가함

---
+++ post-api/dist-electron/preload.mjs
@@ -0,0 +1,9 @@
+"use strict";
+const electron = require("electron");
+electron.contextBridge.exposeInMainWorld("api", {
+  //main process 에 get-posts 요청을 한다 
+  getPosts: () => electron.ipcRenderer.invoke("get-posts"),
+  //main process 에 add-post 요청을 하면서 매개변수에 전달된 추가할 글정보를 전달한다.
+  addPost: (newPost) => electron.ipcRenderer.invoke("add-post", newPost),
+  deletePost: (id) => electron.ipcRenderer.invoke("delete-post", id)
+});
---
+++ post-api/electron/types/index.ts
@@ -0,0 +1,7 @@
+// electron/types/index.ts
+
+export interface Post{
+    readonly id?:number; // 읽기전용 , optional
+    title:string;
+    author:string;
+}
