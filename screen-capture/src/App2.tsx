4a956606172e5605df5941e75d39079a5feaf9c8
kimgura <oli999@naver.com>
2025-04-04 10:33:45 +0900

    screen-capture 에 canva 사각형그리기 예제 완성

--- screen-capture/src/App2.tsx
+++ screen-capture/src/App2.tsx
@@ -11,7 +11,7 @@ function App2(props) {
         height:window.innerHeight
     }
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+    
     //현재 그리기 모드인지 여부
     const [isDrawing, setIsDrawing] = useState(false);
     //선택한 사각형 영역을 state 로 관리하기
@@ -22,14 +22,9 @@ function App2(props) {
         endY: 0
     });
 
-    useEffect(()=>{
-        if(canvasRef.current){
-            const ctx = canvasRef.current.getContext("2d");
-            ctxRef.current = ctx;
-        }
-    }, [isDrawing]);
-
+    //마우스 다운 이벤트가 일어 났을때 
     const handleMouseDown = (e: React.MouseEvent) => {
+        //현재 그리고 있는 상태로 변경한다. 
         setIsDrawing(true);
         const rect = canvasRef.current!.getBoundingClientRect();
     
@@ -48,6 +43,7 @@ function App2(props) {
       };
     
       const handleMouseMove = (e: React.MouseEvent) => {
+        // mouse move 이벤트가 발생하더라도 현재 그리는 중이 아니면 함수를 여기서 종료!
         if (!isDrawing) return;
         const rect = canvasRef.current!.getBoundingClientRect();
     
@@ -63,15 +59,21 @@ function App2(props) {
         const y = Math.min(startY, currentY);
         const width = Math.abs(currentX - startX);
         const height = Math.abs(currentY - startY);
-    
-        const ctx = ctxRef.current;
+        // canvas 에 그림을 그릴 도구의 참조값 얻어와서 
+        const ctx = canvasRef.current?.getContext("2d");
     
         if (ctx) {
+          //  .clearRect(x, y, width, height)  사각형 영역에 그려진 내용을 모두 지우기 
           ctx.clearRect(0, 0, canvasStyle.width, canvasStyle.height);
+          // 체우는 색상 
           ctx.fillStyle = 'rgba(255, 255, 255, 0)';
+          // 체우는 스타일의 사각형 그리기 
           ctx.fillRect(x, y, width, height);
+          // 외곽선 색상
           ctx.strokeStyle = 'red';
+          // 선의 굵기
           ctx.lineWidth = 2;
+          // 외곽선 그리기 
           ctx.strokeRect(x, y, width, height);
         }
     
@@ -84,6 +86,7 @@ function App2(props) {
       };
     
       const handleMouseUp = async () => {
+        //마우스를 떼었을때 다시 상태값을 변경해준다. 
         setIsDrawing(false);
       };
 
